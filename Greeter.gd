d619fb660aeaa46aa7679fbd5938ca3fc09d7678
PakoSt <kokotekilata@gmail.com>
2018-10-06 13:46:21 +0100

    Extend greeting message
    Add two variables
    Use new line to print the new messages

--- Greeter.gd
+++ Greeter.gd
@@ -3,4 +3,7 @@ extends Node2D
 
 func _ready():
 	var person = "you!"
+	var greeting_Message = "What are we waiting for?"
+	var godot_Name = "GODOT!"
 	print ("Hello " + person)
+	print (greeting_Message + "\n" + godot_Name)
